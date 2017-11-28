38840634de25a8621902d0ad5ebdb0e1001f6e1d
Bolei Fu <boleifu@google.com>
2017-11-28 13:23:00 -0800

    Cleanup the OpenConfig struct generating script.

--- openconfig/scripts/generate_wifi_oc_schema.sh
+++ openconfig/scripts/generate_wifi_oc_schema.sh
@@ -16,6 +16,10 @@
 #
 # Generates the latest version of the OpenConfig textproto schema descriptor and
 # model visualization from the source YANG modules.
+#
+# Download OpenConfig models from https://github.com/openconfig/public
+# Download ietf models from https://github.com/openconfig/yang/tree/master/standard/ietf/RFC
+#
 # To use this, run thr scripts directory:
 #    ./generate_wifi_oc_schema.sh
 
@@ -25,19 +29,17 @@ export GOPATH=$HOME/go
 YANG_CONVERTER=$GOPATH/src/github.com/openconfig/ygot/generator/generator.go
 
 # OpenConfig modules
-OC_DIR=$(pwd)../../../../../../../openconfig
-YANG_MODELS=$OC_DIR/public/release/models
-IETF_MODELS=$OC_DIR/yang/standard/ietf/RFC
-WIFI_MODELS=$OC_DIR/wifi
+YANG_MODELS=<path to OpenConfig model>/public/release/models
+IETF_MODELS=<path to OpenConfig model>/yang/standard/ietf/RFC
 OWCA_TOP_MODULE=../models/wifi-office.yang
-IGNORED_MODULES=openconfig-wifi-phy,openconfig-wifi-mac
+IGNORED_MODULES=openconfig-wifi-phy,openconfig-wifi-mac,openconfig-system
 
 # Output path
 OUTPUT_PACKAGE_NAME=ocstruct
 OUTPUT_FILE_PATH=../../generated/$OUTPUT_PACKAGE_NAME/$OUTPUT_PACKAGE_NAME.go
 
 go run $YANG_CONVERTER \
--path=$YANG_MODELS,$IETF_MODELS,$WIFI_MODELS \
+-path=$YANG_MODELS,$IETF_MODELS \
 -generate_fakeroot -fakeroot_name=office \
 -package_name=ocstruct -compress_paths=false \
 -exclude_modules=$IGNORED_MODULES \
